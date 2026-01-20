9d0e48dd612e02c7d0807ca8b52adc18a823ce32
Ito Shun <jacksonis16019207@gmail.com>
2026-01-20 23:24:15 +0900

    GradleビルドツールとSpring Bootプロジェクトの初期設定を追加

---
+++ flowlet/src/main/java/com/example/flowlet/FlowletApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowletApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FlowletApplication.class, args);
+    }
+
+}
