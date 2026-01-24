dcc22ec8a0c3cdf17260f2e6b46038c07ae355f9
Ito Shun <jacksonis16019207@gmail.com>
2026-01-24 14:10:51 +0900

    シーケンス作成スクリプトと設計ドキュメントの削除、およびOpenAPI Generatorビルド設定の追加

--- docs/db/create_sequence.sql
+++
@@ -1,4 +0,0 @@
-create sequence flowlet.account_id_seq maxvalue 9999999999 cycle owned by flowlet.m_account.account_id;
-create sequence flowlet.category_id_seq maxvalue 99999 cycle owned by flowlet.m_category.category_id;
-create sequence flowlet.virtual_account_id_seq maxvalue 9999999999 cycle owned by flowlet.m_virtual_account.virtual_account_id;
-create sequence flowlet.transaction_id_seq maxvalue 9999999999 cycle owned by flowlet.t_transaction.transaction_id;
